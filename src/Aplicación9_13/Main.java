66d58f05a6e47ef6fcf2b88a81b558e2191b1e84
AngelsGV <angels.not99@gmail.com>
2024-04-08 20:16:34 +0200

    Initial commit

--- src/Aplicación9_13/Main.java
+++ src/Aplicación9_13/Main.java
@@ -4,55 +4,55 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+import static java.util.Collections.sort;
+
 public class Main {
     public static void main(String[] args) {
-        // Crear una tabla de futbolistas con 5 futbolistas
+        Scanner sc = new Scanner(System.in);//vamos a tener que introducir datos
+        // Creamos una tabla de futbolistas con 5 futbolistas.
+        //He intentado otra forma de crear la tabla más facilmente
         ArrayList<Futbolista> tablaFutbolistas = new ArrayList<>();
-        tablaFutbolistas.add(new Futbolista("12345678A", "Lionel Messi", 34, 700));
-        tablaFutbolistas.add(new Futbolista("98765432B", "Cristiano Ronaldo", 36, 760));
-        tablaFutbolistas.add(new Futbolista("24681357C", "Neymar Jr", 29, 200));
-        tablaFutbolistas.add(new Futbolista("13579246D", "Robert Lewandowski", 33, 550));
-        tablaFutbolistas.add(new Futbolista("56789123E", "Kylian Mbappé", 23, 300));
 
-        // Crear un objeto Scanner para la entrada del usuario
-        Scanner scanner = new Scanner(System.in);
+        tablaFutbolistas.add(new Futbolista("11111111A", "Messi", 34, 300));
+        tablaFutbolistas.add(new Futbolista("22222222B", "Ronaldo", 36, 900));
+        tablaFutbolistas.add(new Futbolista("333333333C", "Neymar ", 29, 200));
+        tablaFutbolistas.add(new Futbolista("444444444D", "Piqué", 33, 670));
+        tablaFutbolistas.add(new Futbolista("555555555E", "Casillas", 45, 100000));
 
-        // Menú para seleccionar el tipo de ordenamiento
         while (true) {
-            System.out.println("\n Menú de ordenamiento:");
+            System.out.println("\n Menú:");
             System.out.println("1. Ordenar por DNI");
             System.out.println("2. Ordenar por nombre");
             System.out.println("3. Ordenar por edad");
             System.out.println("4. Salir");
             System.out.print("Seleccione una opción: ");
 
-            // Leer la opción seleccionada por el usuario
-            int opcion = scanner.nextInt();
+            int opcion = sc.nextInt();
 
-            // Procesar la opción seleccionada
+
             switch (opcion) {
                 case 1:
                     // Ordenar por DNI
-                    Collections.sort(tablaFutbolistas);
-                    System.out.println("\nTabla de futbolistas ordenada por DNI:");
+                    sort(tablaFutbolistas);
+                    System.out.println("\nTabla ordenada por DNI:");
                     break;
                 case 2:
                     // Ordenar por nombre
-                    Collections.sort(tablaFutbolistas, new Futbolista.ComparadorNombre());
-                    System.out.println("\nTabla de futbolistas ordenada por nombre:");
+                    sort(tablaFutbolistas, new Futbolista.ComparadorNombre());
+                    System.out.println("\nTabla ordenada por nombre:");
                     break;
                 case 3:
                     // Ordenar por edad
-                    Collections.sort(tablaFutbolistas, new Futbolista.ComparadorEdad());
-                    System.out.println("\nTabla de futbolistas ordenada por edad:");
+                    sort(tablaFutbolistas, new Futbolista.ComparadorEdad());
+                    System.out.println("\nTabla ordenada por edad:");
                     break;
                 case 4:
                     // Salir del programa
-                    System.out.println("Saliendo del programa...");
+                    System.out.println("Saliendo...");
                     System.exit(0);
                 default:
                     // Opción inválida
-                    System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                    System.out.println("Opción erronéa. Por favor, seleccione otra.");
                     break;
             }
 
